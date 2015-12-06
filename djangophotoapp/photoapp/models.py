cf84acd5093f1847fc1404d6e294c5da9e2ba3e3
andela-oshodi <olufunmilade.oshodi@andela.com>
2015-12-06 13:18:58 +0100

    (Feature) Created the model class for the pictures

--- djangophotoapp/photoapp/models.py
+++ djangophotoapp/photoapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     photo = models.TextField()
+
+
+class Pictures(models.Model):
+    owner = models.ForeignKey(User)
+    picture = CloudinaryField('picture')
+    picture_file_name = models.CharField(max_length=100, null=True)
