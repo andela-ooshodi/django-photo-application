78317f891cf5eced87f9c6d25e958d6a1a217ba5
andela-oshodi <olufunmilade.oshodi@andela.com>
2015-12-22 13:31:02 +0100

    [Fix] Code Update: Using django delete signals to delete from file system

--- djangophotoapp/photoapp/models.py
+++ djangophotoapp/photoapp/models.py
@@ -1,6 +1,10 @@
 from time import time
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+import os
 
 
 def upload_path(instance, filename):
@@ -22,3 +26,24 @@ class Images(models.Model):
     image_file_name = models.CharField(max_length=100, null=True)
     date_created = models.DateTimeField(
         auto_now_add=True, verbose_name='created')
+
+
+# Function to delete from the file storage
+@receiver(post_delete, sender=Images)
+def delete_from_file_system(sender, instance, **kwargs):
+
+    image_path = instance.image.path
+
+    # split the image part
+    filepath, ext = os.path.splitext(image_path)
+    # create the filtered image path
+    new_filepath = filepath + "filtered" + ext
+    # delete from file directory
+
+    if os.path.exists(image_path):
+        # delete image
+        os.remove(image_path)
+
+    if os.path.exists(new_filepath):
+        # delete filtered image
+        os.remove(new_filepath)
